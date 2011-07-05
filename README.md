52e7fccf123696a21355c55f218ae01d0afa3f13
Cyril Wack <cyril.wack@gmail.com>
2011-07-05 19:59:23 +0200

    open-sourcing the extension

---
+++ README.md
@@ -0,0 +1,17 @@
+Web Technology Notifier
+=======================
+
+Web Technology Notifier displays the technology that is executed by the Web
+server of the current page through an icon.  Many modules and CMS can be
+identified, such as Phusion Passenger for Ruby applications (as Ruby on Rails
+and Sinatra frameworks), PHP based applications (like Zend Server or iPyramid),
+Zope (Python powered), Microsoft ASP.NET, and more.
+
+This notifier can display more information when clicked.
+
+Philosophy
+----------
+
+Being a general extension that does only one thing, without any feature.
+
+Copyright (c) 2010-2011 Cyril Wack, released under the MIT license
---
+++ web_technologies.js
@@ -0,0 +1,104 @@
+var web_technologies = [
+  {
+    "icon": "ipyramid.ico",
+    "title": "iPyramid",
+    "regex": "^iPyramid.*",
+    "url": "http://www.ipyramid.lt/"
+  },
+  {
+    "icon": "hostcms.ico",
+    "title": "Host CMS",
+    "regex": "^HostCMS.*",
+    "url": "http://www.hostcms.ru/"
+  },
+  {
+    "icon": "django.ico",
+    "title": "Django | The Web framework for perfectionists with deadlines",
+    "regex": "^Django.*",
+    "url": "http://djangoproject.com/"
+  },
+  {
+    "icon": "dragonflycms.ico",
+    "title": "CPG Dragonfly CMS",
+    "regex": "Dragonfly CMS using PHP engine",
+    "url": "http://dragonflycms.org/"
+  },
+  {
+    "icon": "cryohost.net.gif",
+    "title": "CryoHost.NET",
+    "regex": "^CryoHost.NET.*",
+    "url": "http://www.wpminternet.com/"
+  },
+  {
+    "icon": "perl.ico",
+    "title": "CGI::WebOut",
+    "regex": "^CGI::WebOut.*",
+    "url": "http://www.dklab.ru/chicken/nablas/4.html"
+  },
+  {
+    "icon": "biferno.ico",
+    "title": "Biferno",
+    "regex": "^Biferno.*",
+    "url": "http://www.tabasoft.it/biferno/"
+  },
+  {
+    "icon": "ez.ico",
+    "title": "eZ Publish",
+    "regex": "^eZ (p|P)ublish.*",
+    "url": "http://ez.no/"
+  },
+  {
+    "icon": "adobe.ico",
+    "title": "Adobe ColdFusion",
+    "regex": "^ColdFusion.*",
+    "url": "http://www.adobe.com/products/coldfusion/"
+  },
+  {
+    "icon": "oracle.ico",
+    "title": "JavaServer Pages Technology",
+    "regex": "^JSP.*",
+    "url": "http://java.sun.com/products/jsp/"
+  },
+  {
+    "icon": "php.ico",
+    "title": "PHP: Hypertext Preprocessor",
+    "regex": "^PHP.*",
+    "url": "http://php.net/"
+  },
+  {
+    "icon": "phusion_passenger.png",
+    "title": "Phusion Passenger",
+    "regex": "^Phusion Passenger.*",
+    "url": "http://www.modrails.com/"
+  },
+  {
+    "icon": "vosandhowden.ico",
+    "title": "Vos & Howden, LLC",
+    "regex": "^Vos & Howden, LLC.*",
+    "url": "http://vosandhowden.com/"
+  },
+  {
+    "icon": "waetech.ico",
+    "title": "WAE Technologies, Inc.",
+    "regex": "^WAE Technologies, Inc.*",
+    "url": "http://www.waetech.com/"
+  },
+  {
+    "icon": "zend_server.ico",
+    "title": "Zend Server",
+    "regex": "^Zend Core.*",
+    "url": "http://www.zend.com/"
+  },
+  {
+    "icon": "zope.ico",
+    "title": "Zope",
+    "regex": "^Zope.*",
+    "url": "http://zope.org/"
+  },
+  {
+    "icon": "asp.net.ico",
+    "title": "ASP.NET",
+    "regex": "^ASP.NET.*",
+    "url": "http://www.asp.net/"
+  }
+];
